847f75e19a3068a0fff9e9e39369cb91150127ad
Levyy331 <guik.levente.2023d@telekimezotur.hu>
2026-09-24 10:47:00 +0200

    1.feladat

--- 01_statikuselemek/Program.cs
+++ 01_statikuselemek/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using _01_statikuselemek;
+
+Console.WriteLine($"eur to huf: {Valutavalto.EurToHuf(100)}");
+Console.WriteLine($"usd to huf: {Valutavalto.UsdToHuf(250)}");
+Console.WriteLine($"Total conversions: {Valutavalto.GetOsszesAtvaltas()}");
