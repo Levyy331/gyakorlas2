5fd2774b7d16d43741d9fa7a2a65f27edb892980
Levyy331 <guik.levente.2023d@telekimezotur.hu>
2026-09-25 10:45:33 +0200

    2.feladat

---
+++ 02_AdatvedelemEsKonstruktorok/Program.cs
@@ -0,0 +1,8 @@
+﻿using _02_AdatvedelemEsKonstruktorok;
+
+Bankszamla Anna = new Bankszamla("100");
+Bankszamla Bela = new Bankszamla("200", 100000, 50000);
+Anna.Befizetes(10000);
+Bela.Befizetes(20000);
+Console.WriteLine($"Anna bankszámla: {Anna.Szamlaszam}, Egyenleg: {Anna.Egyenleg}, Hitelkeret: {Anna.HitelKeret}");
+Console.WriteLine($"Bela bankszámla: {Bela.Szamlaszam}, Egyenleg: {Bela.Egyenleg}, Hitelkeret: {Bela.HitelKeret}");
